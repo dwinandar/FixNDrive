0163b90a7cbc0236f0b361b256681a246f786911
dwinandar <adedwinandar50@gmail.com>
2023-10-06 06:22:15 +0700

    adding no notif page, navbar after login and compressing banner login

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 // PASSWORD TOGGLE SHOW/HIDE
-
 const togglePassword = document.querySelector("#togglePassword");
-
 const password = document.querySelector("#password");
 
-togglePassword.addEventListener("click", function (e) {
+togglePassword?.addEventListener("click", function () {
   const type =
     password.getAttribute("type") === "password" ? "text" : "password";
   password.setAttribute("type", type);
+});
+
+// PROFILE DROPDOWN TOGGLE
+const profileElem = document.querySelector(".profile-wrap");
+const profileDropDown = document.getElementById("profile-dropdown");
+
+profileElem?.addEventListener("click", (e) => {
+  e.stopPropagation();
+
+  profileDropDown.classList.toggle("active");
+  if (profileDropDown.classList.contains("active")) {
+    document.body.onclick = () => {
+      profileDropDown.classList.remove("active");
+    };
+  } else {
+    document.body.onclick = null;
+  }
 });
